c62b698f650e37fd0779531f5afa3a91d9f530e0
Max <184944+montrealist@users.noreply.github.com>
2020-05-27 19:56:00 -0400

    added hard-coded list of applications

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,33 @@
 <script>
 	export let name;
+	export let applications;
 	import './tachyons.css';
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<section class="pv6-ns">
+		<div class="f3 list pl0 mt0 measure-wide-ns center">
+			{#if applications.length === 0}
+				Loading...
+			{:else}
+				{#each applications as app}
+					<li class="flex items-center lh-title pa3 ph0-l bb b--black-10">
+                        <div class="pl3 flex-auto">
+                            <span class="f5 db black-70">{app.company}</span>
+                            <span class="f4 db black-70">{app.position}</span>
+                            <span class="f5 db black-70">{app.notes}</span>
+                        </div>
+                        <div class="flex flex-column">
+                            <a class="w4 f5 tc link dim br3 ba bw1 ph3 pv2 ml2 mb2 dib navy" href="!#">edit</a>
+                            <button class="w4 f5 link dim br3 ba bw1 ph3 pv2 ml2 mb2 dib dark-pink">delete</button>
+                        </div>
+                    </li>
+				{/each}
+			{/if}
+			<!-- <ItemList onDelete={onDelete} items={currentItems} /> -->
+		</div>
+	</section>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,38 @@
 import App from './App.svelte';
 
+const applications =
+[
+	{
+		type: "PhoneInterviewComplete",
+		company: "Foobar Inc.",
+		position: "Star Blazing Software Developer",
+		notes: "This is a really cool job. The interviewer seemed like a really chill person. I know they will get back to me!",
+		createdAt: new Date(),
+		updatedAt: new Date()
+	},
+	{
+		type: "Applied",
+		company: "Some Other Cool Company Inc.",
+		position: "Front End Engineer",
+		notes: "Sent the resume on May 1st, waiting to hear back from them.",
+		createdAt: new Date(),
+		updatedAt: new Date()
+	},
+	{
+		type: "PhoneInterviewScheduled",
+		company: "Element AI",
+		position: "Unicorn Developer",
+		notes: "Phone interview scheduled for May 11th. Need to brush up on Vue JS and MVVM basics.",
+		createdAt: new Date(),
+		updatedAt: new Date()
+	}
+];
+
 const app = new App({
 	target: document.body,
 	props: {
-		name: 'world'
+		name: 'world',
+		applications
 	}
 });
 
