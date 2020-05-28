d2be99fe11f906b2a5b6b1bdb5ec88f2820ca11e
Max <184944+montrealist@users.noreply.github.com>
2020-05-28 19:41:53 -0400

    added edit functionality to form

--- src/components/AddEditForm.svelte
+++ src/components/AddEditForm.svelte
@@ -12,8 +12,9 @@
 		notes: ''
 	};
 
+	let db;
+
 	onMount(async () => {
-		let db;
 
 		if (id) {
 			try {
@@ -28,26 +29,49 @@
 			} finally {
 				db.close();
 			}
-
 		}
 	});
+
+	const onSubmit = async (e) => {
+
+		e.preventDefault();
+
+		if (item.company.trim() === '' || item.position.trim() === '') {
+			return;
+		}
+
+		db = await dbInit().catch(e => {
+			console.errror(e);
+		});
+
+		if (id) {
+			const idToUpdate = parseInt(id, 10);
+			const res = await db[table].update(idToUpdate, item);
+			// TODO: if res === 0 means update happened - show error message 
+		} else {
+			const id = db[table].add(item);
+			console.log('new id', id);
+			// TODO: if id === 0 means insert happened?
+		}
+	};
+
 </script>
 
 <div class="f3 list pl0 mt0 measure-wide-ns center">
-	<form class="pa4 black-80">
+	<form class="pa4 black-80" on:submit={onSubmit}>
 		<div class="measure">
 			<label htmlFor="company" class="f6 b db mb2">Company</label>
-			<input id="company" value={item.company} class="input-reset ba b--black-20 pa2 mb2 db w-100" type="text"
+			<input id="company" bind:value={item.company} class="input-reset ba b--black-20 pa2 mb2 db w-100" type="text"
 				aria-describedby="name-desc" />
 		</div>
 		<div class="measure">
 			<label htmlFor="position" class="f6 b db mb2">Position</label>
-			<input id="position" value={item.position} class="input-reset ba b--black-20 pa2 mb2 db w-100" type="text"
+			<input id="position" bind:value={item.position} class="input-reset ba b--black-20 pa2 mb2 db w-100" type="text"
 				aria-describedby="name-desc" />
 		</div>
 		<div class="measure">
 			<label htmlFor="notes" class="f6 b db mb2">Notes <span class="normal black-60">(optional)</span></label>
-			<textarea id="notes" value={item.notes} name="notes"
+			<textarea id="notes" bind:value={item.notes} name="notes"
 				class="db border-box hover-black w-100 measure ba b--black-20 pa2 br2 mb2"
 				aria-describedby="comment-desc"></textarea>
 			<small id="name-desc" class="f6 black-60 db mb2">Any relevant notes about the company or the
@@ -57,8 +81,9 @@
 			<!-- <StatusMessage {...message} /> -->
 		</div>
 		<div class="measure tr">
-			<button class="f4 link dim br3 ba bw1 ph3 pv2 ml2 mb2 dib dark-green">Submit</button>
-			<button class="f4 link dim br3 ba bw1 ph3 pv2 ml2 mb2 dib mid-gray">{id !== null ? "Cancel" : "Clear"}</button>
+			<button class="f4 link dim br3 ba bw1 ph3 pv2 ml2 mb2 dib dark-green" type="submit">Submit</button>
+			<button class="f4 link dim br3 ba bw1 ph3 pv2 ml2 mb2 dib mid-gray">{id !== null ? "Cancel" :
+				"Clear"}</button>
 		</div>
 
 	</form>
--- src/db.js
+++ src/db.js
@@ -2,6 +2,6 @@ import Dexie from 'dexie';
 
 export default Dexie.async(function* () {
     let db = new Dexie('ApplicationsDB');
-    db.version(1).stores({ applications: '++id' });
+    db.version(1).stores({ applications: '++id' }); // inbound primary key
     return db;
 });
