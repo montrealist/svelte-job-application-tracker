bd2a6e69dd72c0c41c1941a79362797301fd11a1
Max <184944+montrealist@users.noreply.github.com>
2020-05-27 20:32:30 -0400

    added indexedDB and seed.html

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,23 @@
 <script>
+	import { onMount } from 'svelte';
+
 	export let name;
-	export let applications;
 	import './tachyons.css';
 	import ItemList from './components/ItemList.svelte';
+
+	import db from './db';
+
+	let applications = [];
+	const tableName = 'applications';
+
+	onMount(async () => {
+		try {
+			// TODO: dexie insists to use the yield keyword instead of await but Svelte is throwing an error
+		    applications = await db.table(tableName).toArray();
+		} finally {
+		    db.close();
+		}
+	});
 </script>
 
 <main>
