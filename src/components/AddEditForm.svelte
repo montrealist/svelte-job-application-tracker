6652307d4de530b527305f49f1530de7ce192364
Max <184944+montrealist@users.noreply.github.com>
2020-06-01 14:02:21 -0400

    logic to add new item and reset form

--- src/components/AddEditForm.svelte
+++ src/components/AddEditForm.svelte
@@ -5,14 +5,20 @@
 
 	import dbInit from '../db';
 
-	let item = {
-		id: -1,
+	const originalItem = {
+		id: null,
 		company: '',
 		position: '',
 		notes: ''
 	};
 
+	let item = Object.assign({}, originalItem);
+	let itemRef;
+
 	let db;
+
+	const updateItemRef = (newItem) => itemRef = Object.assign({}, newItem);
+	const resetItem = () => item = Object.assign({}, originalItem);
 
 	onMount(async () => {
 
@@ -21,10 +27,11 @@
 				const idToGet = parseInt(id, 10);
 
 				db = await dbInit().catch(e => {
-					console.errror(e);
+					console.warn(e);
 				});
 
 				item = await db[table].get(idToGet); // always pass an integer into get()!
+				updateItemRef(item);
 
 			} finally {
 				db.close();
@@ -32,7 +39,7 @@
 		}
 	});
 
-	const onSubmit = async (e) => {
+	const handleSubmit = async (e) => {
 
 		e.preventDefault();
 
@@ -41,33 +48,54 @@
 		}
 
 		db = await dbInit().catch(e => {
-			console.errror(e);
+			console.warn(e);
 		});
 
-		if (id) {
-			const idToUpdate = parseInt(id, 10);
-			const res = await db[table].update(idToUpdate, item);
-			// TODO: if res === 0 means update happened - show error message 
-		} else {
-			const id = db[table].add(item);
-			console.log('new id', id);
-			// TODO: if id === 0 means insert happened?
+		try {
+			if (id) {
+				const idToUpdate = parseInt(id, 10);
+				const res = await db[table].update(idToUpdate, item);
+				if (res === 1) {
+					updateItemRef(item);
+				} else {
+					// TODO: if res === 0 - show error in UI
+				}
+			} else {
+				delete item.id;
+				const insertedId = await db[table].add(item);
+				resetItem();
+				updateItemRef(originalItem);
+			}
+		} catch (e) {
+			// TODO: show error in UI
+			console.warn(e);
 		}
 	};
+
+	const handleCancel = (e) => {
+		e.preventDefault();
+		// if editing - set item back to itemRef 
+		if (id) {
+			item = Object.assign({}, itemRef);
+		} else {
+			// if new item - reset all fields
+			item = Object.assign({}, originalItem);
+		}
+	}
 
 </script>
 
 <div class="f3 list pl0 mt0 measure-wide-ns center">
-	<form class="pa4 black-80" on:submit={onSubmit}>
+	<form class="pa4 black-80" on:submit={handleSubmit} on:reset={handleCancel}>
 		<div class="measure">
 			<label htmlFor="company" class="f6 b db mb2">Company</label>
-			<input id="company" bind:value={item.company} class="input-reset ba b--black-20 pa2 mb2 db w-100" type="text"
-				aria-describedby="name-desc" />
+			<input id="company" bind:value={item.company} class="input-reset ba b--black-20 pa2 mb2 db w-100"
+				type="text" aria-describedby="name-desc" />
 		</div>
 		<div class="measure">
 			<label htmlFor="position" class="f6 b db mb2">Position</label>
-			<input id="position" bind:value={item.position} class="input-reset ba b--black-20 pa2 mb2 db w-100" type="text"
-				aria-describedby="name-desc" />
+			<input id="position" bind:value={item.position} class="input-reset ba b--black-20 pa2 mb2 db w-100"
+				type="text" aria-describedby="name-desc" />
 		</div>
 		<div class="measure">
 			<label htmlFor="notes" class="f6 b db mb2">Notes <span class="normal black-60">(optional)</span></label>
@@ -82,7 +110,7 @@
 		</div>
 		<div class="measure tr">
 			<button class="f4 link dim br3 ba bw1 ph3 pv2 ml2 mb2 dib dark-green" type="submit">Submit</button>
-			<button class="f4 link dim br3 ba bw1 ph3 pv2 ml2 mb2 dib mid-gray">{id !== null ? "Cancel" :
+			<button class="f4 link dim br3 ba bw1 ph3 pv2 ml2 mb2 dib mid-gray" type="reset">{id !== null ? "Cancel" :
 				"Clear"}</button>
 		</div>
 
